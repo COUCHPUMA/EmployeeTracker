3cb11abaea8f3e9f7e09293bd51584f13417d30f
Richard Katsnelson <rkatsnelson91@gmail.com>
2020-10-27 20:43:29 -0400

    started javascript 🚧

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,3 +2,34 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTable = require("console.table");
 var figlet = require('figlet');
+
+//code from figlet module
+figlet('EMPLOYEE TRACKER!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    //port default - 8080
+    port: 8080,
+
+    // username
+    user: "root",
+
+    //password
+    password: "Ollie2019",
+    database: "employeeDb"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    startApp();
+});
+
+//function for app start
